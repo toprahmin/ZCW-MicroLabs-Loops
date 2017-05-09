1997bdf0cab6d700a6e4c392c17b739738aae5ac
Rahmir Ford <Rahmir.ford@gmail.com>
2017-05-09 12:30:58 -0400

    Numbers lab drills

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
@@ -4,16 +4,34 @@ package io.zipcoder.microlabs.mastering_loops;
 public class Numbers {
 
     public String oneToTen(){
-        return "";
+        String numbers = "oneToTen()\n*** Output ***";
+        for(int i = 1; i < 11; i++){
+            numbers = numbers + "\n" + i;
+        }
+        return numbers;
     }
 
-    public String oddNumbers(){
-        return "";
+    public String oddNumbers() {
+        String oddNumbers = "oddNumbers()\n*** Output ***";
+        for (int i = 1; i < 20; i++) {
+            if (i % 2 != 0) {
+                oddNumbers = oddNumbers + "\n" + Math.pow(i, 2);
+            }
+        }
+        return oddNumbers;
     }
+
+
 
     public String squares(){
-        return "";
+        String squares = "Squares()\n*** Output ***";
+            for(int i = 1; i < 11; i++){
+                squares = squares + "\n" + i * i;
+            }
+        return squares;
     }
+
+
 
     public String random4(){
         return "";
--- src/test/java/io/zipcoder/microlabs/mastering_loops/NumbersTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/NumbersTest.java
@@ -9,7 +9,7 @@ public class NumbersTest {
     public void oneToTenTest(){
         //: Given
         Numbers numbers = new Numbers();
-        String expected = "*** Output ***\noneToTen()\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
+        String expected = "oneToTen()\n*** Output ***\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
 
         //: When
         String actual = numbers.oneToTen();
@@ -21,20 +21,29 @@ public class NumbersTest {
     @Test
     public void oddNumbersTest(){
         //: Given
+        Numbers numbers = new Numbers();
+        String expected = "oddNumbers()\n*** Output ***\n1\n3\n5\n7\n9\n11\n13\n15\n17\n19";
 
         //: When
+        String actual = numbers.oddNumbers();
 
         //: Then
+        Assert.assertEquals("The two strings are equal", expected, actual);
     }
 
     @Test
     public void squaresTest(){
         //: Given
+        Numbers numbers = new Numbers();
+        String expected = "Squares()\n*** Output ***\n1\n4\n9\n16\n25\n36\n49\n64\n81\n100";
 
         //: When
+        String actual = numbers.squares();
 
         //: Then
+        Assert.assertEquals("The two strings are equal", expected, actual);
     }
+
 
     @Test
     public void random4Test(){
