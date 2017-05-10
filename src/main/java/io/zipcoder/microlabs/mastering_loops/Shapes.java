ca394d73fbb01724c61bc440f78136c17226e7d7
Rahmir Ford <Rahmir.ford@gmail.com>
2017-05-10 14:45:12 -0400

    table square tested and complete

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
@@ -17,13 +17,33 @@ public class Shapes {
         return sysout + stars;
     }
 
-    public String tableSquare(){
-        return "";
+    public String tableSquare() {
+        String squareOut = "tableSquare()\n*** Output ***";
+        String numbers = "";
+        for (int i = 1; i <= 4; i++){
+            numbers += "\n" + "|";
+            for(int j = 1; j <= 4; j++){
+                numbers = numbers  + (i * j) + "|";
+            }
+        }
+        return squareOut + numbers;
     }
 
     public String tableSquares(int n){
-        return "";
+        String squareOut = "tableSquares()\n*** Output ***";
+        String numbers = "";
+        for (int i = 1; i <= n; i++){
+            numbers += "\n" + "|";
+            for(int j = 1; j <= n; j++){
+                numbers = numbers  + (i * j) + "|";
+            }
+        }
+        return squareOut + numbers;
+
     }
+
+
+
     public static void main(String[] args) {
         Shapes shapes = new Shapes();
         System.out.println(shapes.triangle(5));
--- src/test/java/io/zipcoder/microlabs/mastering_loops/ShapesTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/ShapesTest.java
@@ -28,9 +28,20 @@ public class ShapesTest {
     @Test
     public void tableSquareTest(){
         //: Given
-        String expected = "| 1 | 2 | 3 | 4 |\n| 2 | 4 | 6 | 8 |\n| 3 | 6 | 9 | 12 |\n| 4 | 8 | 12 | 16 |";
+        String expected = "tableSquare()\n*** Output ***\n|1|2|3|4|\n|2|4|6|8|\n|3|6|9|12|\n|4|8|12|16|";
         //: When
         String actual = shapes.tableSquare();
         //: Then
+        Assert.assertEquals("The functions are equal", expected, actual);
+    }
+
+    @Test
+    public void tableSquaresTest(){
+        //: Given
+        String expected = "tableSquares()\n*** Output ***\n|1|2|3|4|5|\n|2|4|6|8|10|\n|3|6|9|12|15|\n|4|8|12|16|20|\n|5|10|15|20|25|";
+        //: When
+        String actual = shapes.tableSquares(5);
+        //: Then
+        Assert.assertEquals("The functions are equal", expected, actual);
     }
 }
